80807ece9530303e142ba2212a33825c5a88d4a7
Marc Wijesuriya <marcwijesuriya@gmail.com>
2025-05-19 12:30:31 +0530

    cleaned up dataset, added filtering for ultra high priced vehicles

--- model.py
+++ model.py
@@ -1,8 +1,11 @@
 import pandas as pd
+import numpy as np
+import matplotlib.pyplot as plt
 import os
 from sklearn.model_selection import train_test_split
 from sklearn.ensemble import RandomForestRegressor
 import sklearn.metrics as skm
+import seaborn as sns
 
 os.chdir("E:/7th Semester/Machine Learning/")
 
@@ -12,28 +15,36 @@ df["Brand Model"] = df["Brand"] + " " + df["Model"]
 
 df.drop(columns=["Brand", "Model", "Edition"], inplace=True)
 
-print(df)
 
 # List of categorical columns
 categorical_cols = ["Brand Model", "Fuel Type", "Transmission",  "Body Type", "Negotiable"]
 df = pd.get_dummies(df, columns=categorical_cols, drop_first=True)
 
-X = df.drop(columns=["Price Rs."])
+df = df.astype(int)
+
+print(df[df["Price Rs."] > 150000000])
+df = df[df["Price Rs."] <= 100000000]
+
+
 y = df["Price Rs."]
-
-X_train, X_test, y_train, y_test = train_test_split(X, y, test_size=0.2, random_state=42)
-print(X_train)
+X = df.drop(columns=["Price Rs."])
 
 
-print(df.isna().sum())
 
-model = RandomForestRegressor(n_estimators=100, max_depth=12, oob_score=True, random_state=24)
+print(df.dtypes)
+
+X_train, X_test, y_train, y_test = train_test_split(X, y, test_size=0.2, random_state=4)
+
+print(X_train, y_train)
+model = RandomForestRegressor(n_estimators=100, oob_score=True, random_state=24)
 model.fit(X_train, y_train)
 y_pred = model.predict(X_test)
 
+sns.boxplot(df["Price Rs."])
+plt.title("Price Distribution")
+plt.xlabel("Price (Rs.)")
+plt.show()
+
 print("R² Score:", skm.r2_score(y_test, y_pred))
 print("RMSE:", skm.root_mean_squared_error(y_test, y_pred))
-
-
-
 
