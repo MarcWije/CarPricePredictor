cca56432dbaf2b55bb01b7c5866d957b52753a11
Marc Wijesuriya <marcwijesuriya@gmail.com>
2025-05-27 12:52:55 +0530

    Auto commit on 2025-05-27 12:52:55 : Hyperparameter Tuning

--- model.py
+++ model.py
@@ -10,10 +10,6 @@ import sklearn.metrics as skm
 from sklearn.preprocessing import LabelEncoder
 import seaborn as sns
 
-def printa(message_to_print, log_file='output.txt'):
-    print(message_to_print)
-    with open(log_file, 'a') as of:
-        of.write(message_to_print + '\n')
 
 os.chdir("E:/7th Semester/Machine Learning/")
 
@@ -59,23 +55,23 @@ for j in range (1,100):
     if i == 0:
         # max depth = 29 ideally, min_samples_leaf = 2 , n_estimators = 106
         model = RandomForestRegressor(n_estimators=106, oob_score=True, random_state = 51, max_depth = 29, min_samples_leaf = 2)
-        printa("\n" + "Iteration : " + str((j-1)/3))
-        printa("Random Forest Regressor : ")
+        print("\n" + "Iteration : " + str((j-1)/3))
+        print("Random Forest Regressor : ")
 
     elif i == 1:    
         # max_depth = 9 seems ideal so far, learning_Rate = 0.009, n_estimators = 1320
         model = XGBRegressor(n_estimators= 1320, learning_rate= 0.009, max_depth= 9)
-        printa("XGBoost Regressor : ")
+        print("XGBoost Regressor : ")
 
     elif i == 2:
         #model = learning_rate = 0.06,n_estimators = 613
         model = LGBMRegressor(n_estimators= 613, learning_rate= 0.057, verbose = -1)
-        printa("LGBM Regressor : ")
+        print("LGBM Regressor : ")
 
     model.fit(X_train, y_train)
     y_pred = model.predict(X_test)
-    printa("R² Score:", str(skm.r2_score(y_test, y_pred)))
-    printa("RMSE:", str(skm.root_mean_squared_error(y_test, y_pred)))
+    print("R² Score:", skm.r2_score(y_test, y_pred))
+    print("RMSE:", skm.root_mean_squared_error(y_test, y_pred))
 
 plt.scatter(y_test, y_pred, alpha=0.3)
 plt.xlabel("Actual Price")
