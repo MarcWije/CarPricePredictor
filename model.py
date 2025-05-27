fc773cc8b5a11589d2318e6acf1a084eb54ada95
Marc Wijesuriya <marcwijesuriya@gmail.com>
2025-05-27 10:37:50 +0530

    Auto commit on 2025-05-27 10:37:50 : Hyperparameter Tuning

--- model.py
+++ model.py
@@ -48,16 +48,16 @@ X = df.drop(columns=["Price Rs."])
 # Optimal random_state is 47
 X_train, X_test, y_train, y_test = train_test_split(X, y, test_size=0.2, random_state = 47)
 
-for i in range (1,100,5):
+for i in range (1,10):
     
     #model = RandomForestRegressor(n_estimators=100, oob_score=True, random_state = 51, max_depth = i)
-    #max_depth = 9 seems ideal so far
-    j = i/100
+    #max_depth = 9 seems ideal so far, learning_Rate = 0.006
+    j = i/1000
     model = XGBRegressor(n_estimators= 2000, learning_rate= j, max_depth= 9)
     #model = LGBMRegressor(n_estimators= 1000, learning_rate= 0.01, random_state= 51)
     model.fit(X_train, y_train)
     y_pred = model.predict(X_test)
-    print("Iteration: " + str(i))
+    print("Iteration: " + str(j))
     print("R² Score:", skm.r2_score(y_test, y_pred))
     print("RMSE:", skm.root_mean_squared_error(y_test, y_pred))
 
