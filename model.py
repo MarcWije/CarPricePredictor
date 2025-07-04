0fb2b72787a831cebb071047fa38e9bfcfddd035
Marc Wijesuriya <marcwijesuriya@gmail.com>
2025-07-04 14:56:00 +0530

    Auto commit on 2025-07-04 14:55:59 : Documentation

--- model.py
+++ model.py
@@ -1,4 +1,5 @@
 import pandas as pd
+import time
 import numpy as np
 import matplotlib.pyplot as plt
 import os
@@ -68,6 +69,11 @@ param_grid_lgbm = {
     'max_depth': [5, 9, 12]
 }
 
+scoring = {
+    'rmse': 'neg_root_mean_squared_error',
+    'r2': 'r2'
+}
+
 # max depth = 29 ideally, min_samples_leaf = 2 , n_estimators = 106
 rf = RandomForestRegressor(oob_score=True, random_state = 51)
 
@@ -77,23 +83,35 @@ xgb = XGBRegressor(random_state = 51)
 #model = learning_rate = 0.06,n_estimators = 613
 lgbm = LGBMRegressor(random_state = 51, verbose = -1)
 
-grid_rf = GridSearchCV(estimator=rf, param_grid=param_grid_rf, cv=5, scoring='r2', n_jobs=-1)
+start = time.time()
+grid_rf = GridSearchCV(estimator=rf, param_grid=param_grid_rf, cv=5, scoring=scoring, refit='rmse', n_jobs=-1)
 grid_rf.fit(X_train, y_train)
+rf_time = time.time() - start
 
 print("Best Random Forest Params:", grid_rf.best_params_)
-print("Best R² Score:", grid_rf.best_score_)
+print("Best RMSE Score:", -grid_rf.best_score_)
+print(f"Training Time (RF): {rf_time:.2f} seconds\n")
+print("Cross-validated R²:", grid_rf.cv_results_['mean_test_r2'][grid_rf.best_index_])
 
-grid_xgb = GridSearchCV(estimator=xgb, param_grid=param_grid_xgb, cv=5, scoring='r2', n_jobs=-1)
+start = time.time()
+grid_xgb = GridSearchCV(estimator=xgb, param_grid=param_grid_xgb, cv=5, scoring=scoring, refit='rmse', n_jobs=-1)
 grid_xgb.fit(X_train, y_train)
+xgb_time = time.time() - start
 
 print("Best XGBoost Params:", grid_xgb.best_params_)
-print("Best R² Score:", grid_xgb.best_score_)
+print("Best RMSE:", -grid_xgb.best_score_)
+print(f"Training Time (XGB): {xgb_time:.2f} seconds\n")
+print("Cross-validated R²:", grid_xgb.cv_results_['mean_test_r2'][grid_xgb.best_index_])
 
-grid_lgbm = GridSearchCV(estimator=lgbm, param_grid=param_grid_lgbm, cv=5, scoring='r2', n_jobs=-1)
+start = time.time()
+grid_lgbm = GridSearchCV(estimator=lgbm, param_grid=param_grid_lgbm, cv=5, scoring=scoring, refit='rmse', n_jobs=-1)
 grid_lgbm.fit(X_train, y_train)
+lgbm_time = time.time() - start
 
 print("Best LightGBM Params:", grid_lgbm.best_params_)
-print("Best R² Score:", grid_lgbm.best_score_)
+print("Best RMSE Score:", grid_lgbm.best_score_)
+print(f"Training Time (LGBM): {lgbm_time:.2f} seconds")
+print("Cross-validated R²:", grid_lgbm.cv_results_['mean_test_r2'][grid_lgbm.best_index_])
 
 '''plt.scatter(y_test, y_pred1, alpha=0.3, label='Random Forest', color='blue')
 plt.scatter(y_test, y_pred2, alpha=0.3, label='XGBoost', color='green')
