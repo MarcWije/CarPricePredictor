35e0737cb9815e1b82634c7f96a3bf1328087f5e
Marc Wijesuriya <marcwijesuriya@gmail.com>
2025-08-03 15:19:58 +0530

    Seperated prediction and results into two routes

--- app/app.py
+++ app/app.py
@@ -13,42 +13,43 @@ with open("encoders.pkl", "rb") as f:
 
 app = Flask(__name__)
 
-@app.route("/", methods=["GET", "POST"])
+@app.route("/", methods=["GET"])
 def index():
     # The homepage with prediction form
-    if request.method == "GET":
+    return render_template("index.html")
 
-        return render_template("index.html")
+@app.route("/predict", methods=["POST"])
+def predict():
+    
+    brand = request.form.get("brand")
+    car_model = request.form.get("model")
+    fuel_type = request.form.get("fuel-type")
+    transmission = request.form.get("transmission")
+    mileage = int(request.form.get("mileage"))
+    engine_capacity = int(request.form.get("engine-capacity"))
+    yom = int(request.form.get("yom"))
 
-    else:
-        brand = request.form.get("brand")
-        car_model = request.form.get("model")
-        fuel_type = request.form.get("fuel-type")
-        transmission = request.form.get("transmission")
-        mileage = int(request.form.get("mileage"))
-        engine_capacity = int(request.form.get("engine-capacity"))
-        yom = int(request.form.get("yom"))
+    data = {
+        "Brand Model" : brand + " " + car_model, 
+        "Fuel Type" : fuel_type, 
+        "Transmission" : transmission, 
+        "Mileage (km)" : mileage, 
+        "Engine Capacity (cc)" : engine_capacity, 
+        "Year of Manufacture" : yom 
+    }
+    features = []
 
-        data = {
-            "Brand Model" : brand + " " + car_model, 
-            "Fuel Type" : fuel_type, 
-            "Transmission" : transmission, 
-            "Mileage (km)" : mileage, 
-            "Engine Capacity (cc)" : engine_capacity, 
-            "Year of Manufacture" : yom 
-        }
-        features = []
+    features.extend([data["Year of Manufacture"], data["Engine Capacity (cc)"], data["Mileage (km)"]])
 
-        features.extend([data["Year of Manufacture"], data["Engine Capacity (cc)"], data["Mileage (km)"]])
-        for col in ["Brand Model", "Fuel Type", "Transmission"]:
-            val = data[col]
-            if val in encoders[col].classes_:
-                encoded_val = encoders[col].transform([val])[0]
-            else:
-                return jsonify({"error": f"Unknown value for {col}: {val}"}), 400
-            features.append(encoded_val)
+    for col in ["Brand Model", "Fuel Type", "Transmission"]:
+        val = data[col]
+        if val in encoders[col].classes_:
+            encoded_val = encoders[col].transform([val])[0]
+        else:
+            return jsonify({"error": f"Unknown value for {col}: {val}"}), 400
+        features.append(encoded_val)
 
-        price = model.predict([features])
-        price = float(price[0])
+    price = model.predict([features])
+    price = float(price[0])
 
-        return render_template("prediction.html", brand=brand, car_model=car_model, fuel_type=fuel_type, transmission=transmission, mileage=mileage, engine_capacity=engine_capacity, yom=yom, price=price)
+    return render_template("prediction.html", brand=brand, car_model=car_model, fuel_type=fuel_type, transmission=transmission, mileage=mileage, engine_capacity=engine_capacity, yom=yom, price=price)
